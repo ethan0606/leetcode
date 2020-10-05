35ffe2605b0b45facd739f00bc282d661479ea5f
ethan <yguo0606@gmail.com>
2020-10-05 23:22:10 +0800

    updated

--- list/link_list.py
+++ list/link_list.py
@@ -1,6 +1,12 @@
 # Linked list 是一个节点接下一个节点，每个节点保存下一个指向的地址
 
 
+class ListNode:
+    def __init__(self, x):
+        self.x = x
+        self.next = None
+
+
 class Node:
     def __init__(self, val):
         self.val = val
@@ -26,10 +32,22 @@ def generate_list(arr):
     return dummy.next
 
 
-# l1 = Node(0)
-# l1.next = Node(1)
-# l1.next.next = Node(2)
+l1 = Node(0)
+l1.next = Node(1)
+l1.next.next = Node(2)
 # print_list(l1)
-
-
 # print_list(generate_list([1, 2 , 3]))
+
+
+def delete_node(node):
+    if not node.next:
+        node = None
+    next_node = node.next
+    node.val = next_node.val
+    node.next = next_node.next
+    return
+
+print_list(l1)
+delete_node(l1.next)
+print_list(l1)
+
